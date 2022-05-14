4c3f01f4b7dcf40bc61c469564c528d6a5934012
metabloit-m <minjastermoses@gmail.com>
2022-05-14 15:25:40 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home(){
+    return (
+        <>
+            <div className="rounded-xl mx-auto items-center space-x-4 shadow-md max-w-md px-8 py-8 flex justify-center">
+                <div>
+                    <p>Welcome to NextJS + TailwindCSS + mongoose <span className="inline-block text-slate-500">(&copy; metabloit.iO)</span>
+                    </p>
+                </div>
+            </div>
+
+        </>
+    )
+}
